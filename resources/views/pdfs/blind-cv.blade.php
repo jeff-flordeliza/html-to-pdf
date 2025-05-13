8c47d36f2a1f820aa324e32eb310c9fbebfe46df
Jefferson Pablico Flordeliza <33824480+jeff-flordeliza@users.noreply.github.com>
2025-05-13 11:35:32 +0800

    check if skills exists

--- resources/views/pdfs/blind-cv.blade.php
+++ resources/views/pdfs/blind-cv.blade.php
@@ -14,34 +14,36 @@
     <p style="font-size:18px;">{{ $applicant->career_objectives }}</p>
   </div>
 
-  @if(count($applicant->skills) > 0)
-  <div class="skills" style="padding-top: 20px;">
-    <h1 style="color:#9999ff;font-size:20px;font-weight:bold;text-transform:uppercase;border-bottom:3px solid #9999ff;margin-bottom:10px;">Skills</h1>
-    <table class="w-full">
-      <thead class="ltr:text-left rtl:text-right bg-slate-100 text-slate-500 dark:text-zink-200 dark:bg-zink-600">
-        <tr>
-          <th class="px-3.5 py-2.5 font-semibold border-b border-slate-200 dark:border-zink-500">Skills</th>
-          <th class="px-3.5 py-2.5 font-semibold border-b border-slate-200 dark:border-zink-500 text-center">Years of Experience</th>
-          <th class="px-3.5 py-2.5 font-semibold border-b border-slate-200 dark:border-zink-500">Proficiency Level</th>
-          <th class="px-3.5 py-2.5 font-semibold border-b border-slate-200 dark:border-zink-500 text-center">Last Year Used</th>
-        </tr>
-      </thead>
-      <tbody>
-        @forelse ($applicant->skills as $skill)
-        <tr>
-          <td class="px-3.5 py-2.5 border border-slate-200 dark:border-zink-500">{{ $skill->skill }}</td>
-          <td class="px-3.5 py-2.5 border border-slate-200 dark:border-zink-500 text-center">{{ $skill->years_of_experience }}</td>
-          <td class="px-3.5 py-2.5 border border-slate-200 dark:border-zink-500">{{ $skill->proficiency_level }}</td>
-          <td class="px-3.5 py-2.5 border border-slate-200 dark:border-zink-500 text-center">{{ $skill->last_year_used }}</td>
-        </tr>
-        @empty
-        <tr>
-          <td colspan="4">No Skills Provided.</td>
-        </tr>
-        @endforelse
-      </tbody>
-    </table>
-  </div>
+  @if(isset($applicant->skills))
+    @if(count($applicant->skills) > 0)
+    <div class="skills" style="padding-top: 20px;">
+      <h1 style="color:#9999ff;font-size:20px;font-weight:bold;text-transform:uppercase;border-bottom:3px solid #9999ff;margin-bottom:10px;">Skills</h1>
+      <table class="w-full">
+        <thead class="ltr:text-left rtl:text-right bg-slate-100 text-slate-500 dark:text-zink-200 dark:bg-zink-600">
+          <tr>
+            <th class="px-3.5 py-2.5 font-semibold border-b border-slate-200 dark:border-zink-500">Skills</th>
+            <th class="px-3.5 py-2.5 font-semibold border-b border-slate-200 dark:border-zink-500 text-center">Years of Experience</th>
+            <th class="px-3.5 py-2.5 font-semibold border-b border-slate-200 dark:border-zink-500">Proficiency Level</th>
+            <th class="px-3.5 py-2.5 font-semibold border-b border-slate-200 dark:border-zink-500 text-center">Last Year Used</th>
+          </tr>
+        </thead>
+        <tbody>
+          @forelse ($applicant->skills as $skill)
+          <tr>
+            <td class="px-3.5 py-2.5 border border-slate-200 dark:border-zink-500">{{ $skill->skill }}</td>
+            <td class="px-3.5 py-2.5 border border-slate-200 dark:border-zink-500 text-center">{{ $skill->years_of_experience }}</td>
+            <td class="px-3.5 py-2.5 border border-slate-200 dark:border-zink-500">{{ $skill->proficiency_level }}</td>
+            <td class="px-3.5 py-2.5 border border-slate-200 dark:border-zink-500 text-center">{{ $skill->last_year_used }}</td>
+          </tr>
+          @empty
+          <tr>
+            <td colspan="4">No Skills Provided.</td>
+          </tr>
+          @endforelse
+        </tbody>
+      </table>
+    </div>
+    @endif
   @endif
 
   @if(isset($applicant->training_and_certifications))
